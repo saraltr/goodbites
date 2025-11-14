b5ff16d758fae8ace012f2ab01ba0b2a14b0b345
Abimael <tavares@abimael.dev.br>
2025-11-14 21:35:21 +0100

    update the middleware

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const session = req.cookies.get('__session');
   const { pathname } = req.nextUrl;
   const url = req.nextUrl.clone();

78a73cd93f0a46ea7f9170aecb0f2577d99b07c6
sara latorre <saralatorre97@gmail.com>
2025-11-22 00:07:54 +0100

    switched images.domains to remotePattern

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.themealdb.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
